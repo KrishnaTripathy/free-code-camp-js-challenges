48cf2c42dbec28f01416309a0634856b839aec32
Krishna Tripathy <98588847+KrishnaTripathy@users.noreply.github.com>
2024-07-18 00:10:18 +0530

    Add files via upload

---
+++ freecodecamp js practice/step7/index.js
@@ -0,0 +1,8 @@
+// Now log your character variable to the console again. You should see the string "Hello", then the string "World", in the console.
+let character = 'Hello';
+console.log(character);
+character = "World";
+console.log(character);
+
+// op: Hello
+    // World
